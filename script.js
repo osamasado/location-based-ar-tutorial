12e0df58e07fc932d4e2c456c5722e31a4583be2
Osama Sado <osama.sadow@gmail.com>
2021-05-06 12:18:48 +0200

    66

--- script.js
+++ script.js
@@ -49,6 +49,7 @@ window.onload = () => {
                     placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
                     placeText.setAttribute('title', place.name);
                     placeText.setAttribute('scale', '15 15 15');
+                    placeText.setAttribute('href', 'https://www.die-etagen.de');
                     
                     placeText.addEventListener('loaded', () => {
                         window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))

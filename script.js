cfb2a203fdbefd790c5bcf658bc2c3d93e64f5e2
Osama Sado <osama.sadow@gmail.com>
2021-05-06 12:16:31 +0200

    666

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // getting places from APIs
 function loadPlaces(position) {
     const params = {
-        radius: 300,    // search places not farther than this value (in meters)
+        radius: 50,    // search places not farther than this value (in meters)
         clientId: 'FNGEK3NYNNVGR3QD150OJDNJKRUOSGNIPSMOPIMKCYWUCF0A',
         clientSecret: '1NJ4YVSVKMV5LB022RBU3K4EL2W5XKTB5WW3C1GDGHUFJYAC',
         version: '20300101',    // foursquare versioning, required but unuseful for this demo
